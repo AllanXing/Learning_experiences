cc8da758557993bf90e2539bb84d605b7d31d8a5
xingpeng <xingpeng@ubuntu.ubuntu-domain>
2012-10-25 20:49:19 +0800

    uppdate

--- java/ch03/src/Data.java
+++ java/ch03/src/Data.java
@@ -3,26 +3,81 @@ package com.xing.ch03;
 public class Data{
 	int first;
 	int second;
-	public void Data(int aFirst,int aSecond)
+	public Data(int aFirst,int aSecond)
 	{
 		first = aFirst;
 		second = aSecond;
 	}
-	public void outBin(int k)
+	public void displayBin(int d)
 	{
 		int i = 31;
 		for(;i >= 0;i--)
 		{
-			k = (k >> i) & 1;
+			int k = (d >> i) & 1;
 			System.out.print(k);
 		}
 		System.out.println();
 	}
-	public void BinAnd()
+	public void displayBitAnd()
 	{
 		System.out.print("first:" + first + ":");
+		displayBin(first);
 		System.out.print("second:" + second + ":");
+		displayBin(second);
 		int res = first & second;
 		System.out.print("res:" + res + ":");
+		displayBin(res);
 	}
+	public void displayBitOr()
+	{
+		System.out.print("first:" + first + ":");
+		displayBin(first);
+		System.out.print("second:" + second + ":");
+		displayBin(second);
+		int res = first | second;
+		System.out.print("res:" + res + ":");
+		displayBin(res);
+	}
+	public void displayBitXor()
+	{
+		System.out.print("first:" + first + ":");
+		displayBin(first);
+		System.out.print("second:" + second + ":");
+		displayBin(second);
+		int res = first ^ second;
+		System.out.print("res:" + res + ":");
+		displayBin(res);
+	}
+	public void displayLeftShiftSym()
+	{
+		System.out.print("num  :" + first + ":");
+		displayBin(first);
+		System.out.print("shift:" + second + ":");
+		displayBin(second);
+		int res = first << second;
+		System.out.print("res  :" + res + ":");
+		displayBin(res);
+	}
+	public void displayRightShiftNoSym()
+	{
+		System.out.print("num  :" + first + ":");
+		displayBin(first);
+		System.out.print("shift:" + second + ":");
+		displayBin(second);
+		int res = first >>> second;
+		System.out.print("res  :" + res + ":");
+		displayBin(res);
+	}
+	public void displayRightShiftSym()
+	{
+		System.out.print("num  :" + first + ":");
+		displayBin(first);
+		System.out.print("shift:" + second + ":");
+		displayBin(second);
+		int res = first >> second;
+		System.out.print("res  :" + res + ":");
+		displayBin(res);
+	}
+
+
 }
--- java/ch03/src/DataTest.java
+++ java/ch03/src/DataTest.java
@@ -1,9 +1,26 @@
 package com.xing.ch03;
 
-public static void main(String[] args){
-	Data data = new Data(3,-5);
+public class DataTest{
+	public static void main(String[] args){
+		Data data = new Data(3,5);
+		Data data1 = new Data(-5,2);
 
-	data.outBin();
-	data.BinAnd();
-	System.out.println();
+		System.out.println("--bit and");
+		data.displayBitAnd();
+
+		System.out.println("--bit or");
+		data.displayBitOr();
+
+		System.out.println("--bit xor");
+		data.displayBitXor();
+
+		System.out.println("--bit <<");
+		data1.displayLeftShiftSym();
+
+		System.out.println("--bit >>>");
+		data1.displayRightShiftNoSym();
+
+		System.out.println("--bit >>");
+		data1.displayRightShiftSym();
+	}
 }
